d4b109a26c35a2419eb83a97490484be97e0a8aa
Emad Deym <47889899+darkdoomrag3@users.noreply.github.com>
2023-05-22 20:34:41 +0330

    importing libraries

---
+++ code.py
@@ -0,0 +1,8 @@
+import numpy as np                               
+import pandas as pd                               
+import seaborn as sns
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')
